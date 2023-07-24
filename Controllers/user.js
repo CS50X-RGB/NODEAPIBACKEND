79fdf4c81d9ad9063b19b66bbd654d23a5dd498e
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-07-25 02:28:23 +0530

    PORT CHANGED

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,44 +1,64 @@
 import User from "../Models/user.js";
 
-export const getAllUser = async(req,res)=>{
-    const users = await User.find({});
-    // params
-    
-    // const key = req.query.keyword;
+export const getAllUser = async (req, res) => {
+  const users = await User.find({});
+  // params
 
-    res.json({
-        sucess:true,
-        users,
-    })
-}
-export const register = async(req,res)=>{
-    const {name,Email,password} = req.body;
-    // will not work need middleware of json 
-    const users = await User.create({
-        name,
-        Email,
-        password
-    });
-    // 201 is for creation
-    res.status(201).cookie('tempo','hiiz').json({
-        sucess:true,
-        mesaage:"Sucessful Regsitration",
-    })
-}
+  // const key = req.query.keyword;
 
-export const specialUser = (req,res)=>{
-    res.json({
-        sucess:"true",
-        message:"Just Joking",
-    })
-}
+  res.json({
+    sucess: true,
+    users,
+  });
+};
+export const register = async (req, res) => {
+  const { name, Email, password } = req.body;
+  // will not work need middleware of json
+  const users = await User.create({
+    name,
+    Email,
+    password,
+  });
+  // 201 is for creation
+  res.status(201).cookie("tempo", "hiiz").json({
+    sucess: true,
+    mesaage: "Sucessful Regsitration",
+  });
+};
 
-export const userById = async (req,res)=>{
-    const { id } = req.params;
-    const user = await User.findById(id);
+export const specialUser = (req, res) => {
+  res.json({
+    sucess: "true",
+    message: "Just Joking",
+  });
+};
 
-    res.json({
-        sucess:"true",
-        user,
-    })
-}
+export const userById = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+
+  res.json({
+    sucess: "true",
+    user,
+  });
+};
+export const updateData = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+
+  res.json({
+    sucess: "true",
+    mesaage: "Updated",
+  });
+};
+export const deleteData = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+ 
+  await user.deleteOne();
+
+  res.json({
+    sucess: "true",
+    mesaage: "Delted User",
+  });
+};
--- Router/user.js
+++ Router/user.js
@@ -1,16 +1,33 @@
-import express from 'express';
-import User from '../Models/user.js';
-import { getAllUser, register, specialUser, userById } from'../Controllers/user.js';
+import express from "express";
+import User from "../Models/user.js";
+import {
+  deleteData,
+  getAllUser,
+  register,
+  specialUser,
+  updateData,
+  userById,
+} from "../Controllers/user.js";
 
 const router = express.Router();
 
-router.get('/all',getAllUser);
+router.get("/all", getAllUser);
 
-router.get('/new',register);
+router.get("/new", register);
 
-router.get('/special',specialUser);
+router.get("/special", specialUser);
+
+router
+  .route("/userId/:userID")
+  .get(userById)
+  .put(updateData)
+  .delete(deleteData);
 
 // ALWAYS KEEP DYNAMIC ROUTING AT END BECUASE FROM TOP JS WILL MAKE THIS ROUTE FIRST
-router.get('/userId/:userID',userById);
+// router.get("/userId/:userID", userById);
+
+// router.put("/userId/:userID", updateData);
+
+// router.delete("/userId/:userID", deleteData);
 
 export default router;
