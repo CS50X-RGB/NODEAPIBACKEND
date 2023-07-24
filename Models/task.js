e8c93ad41a5cbf050622810ae84dce696d38674f
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-07-25 03:06:25 +0530

    Added new packages

---
+++ Models/task.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required:true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Task = mongoose.model("Task", schema);
+export default Task;
--- Models/user.js
+++ Models/user.js
@@ -1,9 +1,24 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: String,
-  Email: String,
-  password: String,
+  name: {
+    type:String,
+    required:true,
+  },
+  Email: {
+    type:String,
+    required:true,
+    unique:true,
+  },
+  password: {
+    type: String,
+    required:true,
+    select: false,
+  },
+  createdAt :{
+    type:Date,
+    default:Date.now,
+  }
 });
 
 const User = mongoose.model("User", schema);
