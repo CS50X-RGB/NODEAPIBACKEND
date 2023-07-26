3786274799b2d9986334c44f6659df8224c3bb67
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-07-27 04:20:51 +0530

    Moongose VErsion updated

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import connectDB from './data/database.js';
 
 connectDB();
 
-app.listen(3001,() => {
-    console.log(`Server is running on port  ${process.env.PORT} in ${process.env.NODE_ENV}`)
+app.listen(2001,() => {
+    console.log(`Server is running on port  2001 in ${process.env.NODE_ENV}`)
 })
